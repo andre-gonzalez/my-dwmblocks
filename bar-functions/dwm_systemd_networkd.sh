de87711c0a7bd9bb069a6e816d45ef783b82b8ca
André Gonzalez <lopescg@gmail.com>
2022-06-11 13:49:45 -0300

    corrected a bug when multiple interfaces are available

--- bar-functions/dwm_systemd_networkd.sh
+++ bar-functions/dwm_systemd_networkd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-interface=$(ip addr | awk '/state UP/ {gsub(":","");print $2}')
+interface=$(ip addr | awk '/state UP/ {gsub(":","");print $2}' | head -n 1)
 status=$(iwctl station $interface show | grep State | grep -o 'connected\|disconnected')
 network=$(iwctl station wlan0 show | grep -Po 'Connected network \K.*' | awk '{$1=$1;print}')
 strength=$(iw dev "$interface" link | awk '/signal/ {gsub("-",""); print $2}')
