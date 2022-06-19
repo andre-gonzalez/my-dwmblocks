845a002ff4e83c6e33aa216a85e96de9bea12d98
André Gonzalez <lopescg@gmail.com>
2022-06-19 11:27:40 -0300

    updated network status to show when connected to ethernet

--- bar-functions/dwm_systemd_networkd.sh
+++ bar-functions/dwm_systemd_networkd.sh
@@ -2,14 +2,14 @@
 
 interface=$(ip addr | awk '/state UP/ {gsub(":","");print $2}' | head -n 1)
 status=$(iwctl station $interface show | grep State | grep -o 'connected\|disconnected')
-network=$(iwctl station wlan0 show | grep -Po 'Connected network \K.*' | awk '{$1=$1;print}')
-strength=$(iw dev "$interface" link | awk '/signal/ {gsub("-",""); print $2}')
+wired=$(ip -s link show enp2s0 | grep -c 'state UP')
 
-case $status in
-		"connected")
-				echo " ""$network" "$strength%"
-		;;
-		*)
-				echo " " "OFFLINE"
-		;;
-esac
+if [ "$status" = "connected" ]; then
+		network=$(iwctl station wlan0 show | grep -Po 'Connected network \K.*' | awk '{$1=$1;print}')
+		strength=$(iw dev "$interface" link | awk '/signal/ {gsub("-",""); print $2}')
+		echo " ""$network""$strength%"
+elif [ "$wired" = 1 ]; then
+		echo " Ethernet"
+else
+		echo " OFFLINE"
+fi
