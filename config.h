998628e2cf703a6b0c7b65a0ef800f713e1a3f5d
André Gonzalez <lopescg@gmail.com>
2023-02-28 14:44:56 -0300

    paused dwm_currency cause the website is having problems

--- config.h
+++ config.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	/* {"⌨", "sb-kbselect", 0, 30}, */
 	{"",	"dwm_cron",	14400, 0},
 	{"",	"dwm_ufw",	60, 0},
-	{"",	"dwm_currency",	14400, 0},
+	/* {"",	"dwm_currency",	14400, 0}, */
 	{"",	"dwm_weather",	14400, 0},
 	{"",	"dwm_packages",	14400, 13},
 	{"",	"dwm_spotify", 3,  0},
