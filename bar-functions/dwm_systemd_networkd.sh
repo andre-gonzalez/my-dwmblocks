ac1005f665dd1e9cf7ca4b7372ac9e1f7dd72dd0
André Gonzalez <lopescg@gmail.com>
2024-11-10 07:51:48 -0600

    Add interface enp0s20f0u2c to the dwm_systemd_networkd script

--- bar-functions/dwm_systemd_networkd.sh
+++ bar-functions/dwm_systemd_networkd.sh
@@ -3,13 +3,14 @@
 interface=$(ip addr | awk '/state UP/ {gsub(":","");print $2}' | grep -v 'virbr0' | head -n 1)
 status=$(iwctl station $interface show | grep State | grep -o 'connected\|disconnected')
 wired=$(ip -s link show enp2s0 | grep -c 'state UP')
+wired_enp0s20f0u2c2=$(ip -s link show enp0s20f0u2c2 | grep -c 'state UP')
 
 if [ "$status" = "connected" ]; then
 		network=$(iwctl station wlan0 show | grep -Po 'Connected network \K.*' | awk '{$1=$1;print}')
 		strength=$(iw dev "$interface" link | awk '/signal/ {gsub("-",""); print $2}')
 		echo " ""$network" "$strength%"
-elif [ "$wired" = 1 ]; then
-		echo "󰈁 Ethernet"
+elif [ "$wired_enp2s0" = 1 ] || [ "$wired_enp0s20f0u2c2" = 1 ]; then
+    echo "󰈁 Ethernet"
 else
 		echo " OFFLINE"
 fi
